a435ae4493654444536824ccf2041bab5f1f7646
jiahuigeng <jiahui.geng@rwth-aachen.de>
2022-08-16 13:46:01 +0200

    delete some version number

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqldb://root:@localhost:8900/uis_summer"
+DATABASE_URL = "mysql+mysqldb://root:@localhost:3306/uis_summer"
 
 db_engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
